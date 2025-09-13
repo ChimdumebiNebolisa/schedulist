fedb375964d59d1831d00273416091ac5d741a1e
Chimdumebi Nebolisa <130222774+ChimdumebiNebolisa@users.noreply.github.com>
2025-09-13 17:14:10 -0500

    Restrict task actions to owning user and allow nullable email

--- models.py
+++ models.py
@@ -13,11 +13,8 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True, nullable=False)
 
-    google_id = db.Column(db.String, unique=True, nullable=False)
-=======
     google_id = db.Column(db.String(255), unique=True, nullable=False)
-    email = db.Column(db.String(255), unique=True, nullable=False)
-
+    email = db.Column(db.String(255), unique=True, nullable=True)
 
     tasks = db.relationship('Task', backref='user', lazy=True)
 
